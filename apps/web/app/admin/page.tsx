5f408fefa41dd2441c632c0cd0feffc16f8e12b0
nitinshukla <nitin.shukla12004@gmail.com>
2025-11-15 17:30:51 +0530

    added admin page

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,9 @@
+import Admin from "@repo/ui/admin"
+export default function () {
+    return (
+        <div>
+            Hi there !
+            <Admin />
+        </div>
+    )
+}
---
+++ packages/ui/src/admin.tsx
@@ -0,0 +1,5 @@
+"use client";
+const Admin = () => {
+    return <h1> Hello from Admin Page</h1>
+}
+export default Admin;
